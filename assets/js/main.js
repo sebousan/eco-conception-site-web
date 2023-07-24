12b18784134ac0aca98f493daf2759a4106cd1ef
sebousan <sebousan@gmail.com>
2023-07-24 12:37:17 +0200

    js

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Vendors
+// import './vendors/bootstrap';
+// import './vendors/cookie';
+
+// Blocks
+// import './blocks/chart';
+// import './blocks/gallery';
+// import './blocks/instagram';
+// import './blocks/map';
+
+// Components
+import './components/animation';
+// import './components/carousel';
+// import './components/gauge';
+// import './components/stickyNav';
+import './components/toc';
